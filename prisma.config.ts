0197f1372ed32aa3a1a19e4de38b62e49396f123
SrCreison <87103932+SrCreison@users.noreply.github.com>
2026-04-13 20:04:54 -0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from '@prisma/config';
+const dotenv = require('dotenv');
+dotenv.config();
 
-export default defineConfig({
+module.exports = {
   schema: './prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL,
   },
-});
+};
